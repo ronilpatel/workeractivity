8444548702f1743d8a958f01d1bccf6e71da530c
Ronil Patel <ronil.star111@gmail.com>
2020-09-10 12:33:20 +0530

    Update populateworker.py

--- worker/management/commands/populateworker.py
+++ worker/management/commands/populateworker.py
@@ -1,24 +1,17 @@
 from django.core.management.base import BaseCommand
-
 from faker import Faker
-
 from worker.models import WorkerProfile
 
 
 def delete_previous_profiles():
-    
     profiles = WorkerProfile.objects.all()
-
     for profile in profiles:
         profile.delete()
 
 
 class Command(BaseCommand):
-
     help = 'Populate the database....'
-
     def add_arguments(self, parser):
-
         parser.add_argument('--users',
                             default=50,
                             type=int,
@@ -27,20 +20,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('populating....')
         faker_obj = Faker()
-
         # delete_previous_profiles()
-
         for _ in range(options['users']):   
-            
             firstname = faker_obj.first_name()
             lastname = faker_obj.last_name()
             timezn = faker_obj.timezone()
-
             wpf = WorkerProfile(firstname=firstname,
                                 lastname=lastname,
                                 timezn=timezn)
-
             wpf.save()
         print('done....')
-
-
